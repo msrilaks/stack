33d1cf63aa895589cff7405c802928fd10ef921e
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2019-05-30 19:52:50 -0700

    Oauth2

--- stack-task-service/src/main/java/com.stack/taskservice/configuration/SecurityConfig.java
+++ stack-task-service/src/main/java/com.stack/taskservice/configuration/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
             .anyRequest().authenticated()
             .and()
             .oauth2Login();
