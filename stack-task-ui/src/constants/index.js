1a7af0061619c7200d45e54571b9264b178c0197
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2019-07-03 16:08:38 -0700

    HTTPS for server

--- stack-task-ui/src/constants/index.js
+++ stack-task-ui/src/constants/index.js
@@ -1,17 +1,17 @@
 const dev = {
   ACCESS_TOKEN: 'accessToken',
     API_BASE_URL: 'https://localhost:8080',
-    OAUTH2_REDIRECT_URI: 'https://localhost:3000/oauth2/redirect',
     GOOGLE_AUTH_URL: 'https://localhost:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://localhost:3000/oauth2/redirect',
+    // OAUTH2_REDIRECT_URI: 'http://localhost:3000/oauth2/redirect',
     // FACEBOOK_AUTH_URL: 'http://localhost:8080' + '/oauth2/authorize/facebook?redirect_uri=' + 'http://localhost:3000/oauth2/redirect',
     // GITHUB_AUTH_URL: 'http://localhost:8080' + '/oauth2/authorize/github?redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
   };
 
   const prod = {
     ACCESS_TOKEN: 'accessToken',
-    API_BASE_URL: 'http://www.stackitdown.com:8080',
-    OAUTH2_REDIRECT_URI: 'http://www.stackitdown.com/oauth2/redirect',
-    GOOGLE_AUTH_URL: 'http://www.stackitdown.com:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://www.stackitdown.com/oauth2/redirect',
+    API_BASE_URL: 'https://www.stackitdown.com:8080',
+    GOOGLE_AUTH_URL: 'https://www.stackitdown.com:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://www.stackitdown.com/oauth2/redirect',
+    // OAUTH2_REDIRECT_URI: 'http://www.stackitdown.com/oauth2/redirect',  
     // FACEBOOK_AUTH_URL: 'http://www.stackitdown.com:8080' + '/oauth2/authorize/facebook?redirect_uri=' + 'http://www.stackitdown.com/oauth2/redirect',
     // GITHUB_AUTH_URL: 'http://www.stackitdown.com:8080' + '/oauth2/authorize/github?redirect_uri=' + 'http://www.stackitdown.com/oauth2/redirect'
 
