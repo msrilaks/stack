15a9359e403459b2de18f58f2314b385c8c83165
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2019-11-11 17:19:38 -0800

    Photo upload

--- stack-services/stack-task-service/commands.bat
+++ stack-services/stack-task-service/commands.bat
@@ -3,5 +3,5 @@ docker rm stack-task-service
 docker network create my-net
 call gradlew clean build
 docker build -t srilakshmi29/stack-task-service:latest .
-REM docker push srilakshmi29/stack-task-service:latest
+docker push srilakshmi29/stack-task-service:latest
 docker run -p 8080:8080 -e LOG_DIR=logs -e SPRING_PROFILES_ACTIVE=develop --net my-net --name stack-task-service -d srilakshmi29/stack-task-service:latest
