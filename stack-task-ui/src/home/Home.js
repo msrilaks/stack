f683ea6e2ce8bc30678552fc8b5d9cb886a298f1
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2019-11-15 18:48:09 -0800

    Home page

--- stack-task-ui/src/home/Home.js
+++ stack-task-ui/src/home/Home.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
+const useStyles = makeStyles(theme => ({
+    gridList: {
+        width: '100%',
+        height: 'auto',
+    },
+
+}));
 class Home extends Component {
+    constructor() {
+        super();
+        this.StackHome = this.StackHome.bind(this);
+    }
+
+    StackHome(){
+        const classes = useStyles();
+        return <div>
+                           <GridList cellHeight={'auto'} className={classes
+                           .gridList} cols={1}>
+                               <GridListTile key='stackHome1' cols={1}>
+                                       <img src="/stackHome1M.jpg" />
+                               </GridListTile>
+                               <GridListTile key='stackHome2' cols={1}>
+                                       <img src="/stackHome2M.jpg" height="600"
+                                             width="800"/>
+                               </GridListTile>
+                           </GridList>
+                       </div>
+    }
+
     render() {
         return (
-            <div className="home-container">
-                <div className="container">
-                    <div className="graf-bg-container">
-                        <div className="graf-layout">
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                            <div className="graf-circle"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           <this.StackHome/>
         )
     }
 }
