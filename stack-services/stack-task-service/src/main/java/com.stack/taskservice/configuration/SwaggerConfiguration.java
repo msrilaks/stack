2152e32f0c860c3b9b854d72f867925cdc850111
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2020-01-07 20:28:41 -0800

    fix:Swagger causing oauth error

--- stack-services/stack-task-service/src/main/java/com.stack/taskservice/configuration/SwaggerConfiguration.java
+++ stack-services/stack-task-service/src/main/java/com.stack/taskservice/configuration/SwaggerConfiguration.java
@@ -2,28 +2,18 @@ package com.stack.taskservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import java.util.*;
-import javax.servlet.ServletContext;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration extends WebMvcConfigurationSupport {
-    private final ServletContext servletContext;
-
-    @Autowired
-    public SwaggerConfiguration(ServletContext servletContext) {
-        this.servletContext = servletContext;
-    }
-
+public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         Set<String> protoSet = new HashSet<String>();
@@ -34,19 +24,6 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
             .apis(RequestHandlerSelectors
             .basePackage("com.stack.taskservice"))
             .paths(PathSelectors.any()).build()
-            .protocols(protoSet)
-            .pathProvider(new RelativePathProvider(servletContext) {
-                @Override
-                    public String getApplicationBasePath() {
-                    return "/";
-                }
-            });
-    }
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+            .protocols(protoSet);
     }
 }
