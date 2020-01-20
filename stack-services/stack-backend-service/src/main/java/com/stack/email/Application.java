4b1abd9c10a9493389f39002402619dc80c55763
msrilaks <srilakshmi.mudaliar@gmail.com>
2020-01-19 16:25:02 -0800

    stack backend pub sub

--- stack-services/stack-backend-service/src/main/java/com/stack/email/Application.java
+++ stack-services/stack-backend-service/src/main/java/com/stack/email/Application.java
@@ -3,6 +3,15 @@ package com.stack.email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
+
+import org.springframework.messaging.MessageChannel;
+
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
 
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active}.properties")
@@ -11,5 +20,23 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public PubSubInboundChannelAdapter messageChannelAdapter(
+            @Qualifier("stackEmailChannel") MessageChannel inputChannel,
+            PubSubTemplate pubSubTemplate) {
+        PubSubInboundChannelAdapter adapter =
+                new PubSubInboundChannelAdapter(pubSubTemplate, "STACK_EMAIL_SUBSCRIPTION");
+        adapter.setOutputChannel(inputChannel);
+        return adapter;
+    }
+    @Bean
+    public MessageChannel myInputChannel() {
+        return new DirectChannel();
+    }
+    @ServiceActivator(inputChannel = "stackEmailChannel")
+    public void messageReceiver(String payload) {
+        System.out.println("### SRI Message arrived! Payload: " + payload);
     }
 }
