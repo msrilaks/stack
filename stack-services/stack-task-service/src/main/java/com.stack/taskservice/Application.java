ad1e06fa1dcc6bd6a923ea743798368989633755
msrilaks <srilakshmi.mudaliar@gmail.com>
2020-02-18 20:09:38 -0800

    redis

--- stack-services/stack-task-service/src/main/java/com.stack/taskservice/Application.java
+++ stack-services/stack-task-service/src/main/java/com.stack/taskservice/Application.java
@@ -8,12 +8,14 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import com.stack.taskservice.configuration.Channels;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active}.properties")
 @EnableMongoRepositories
 @EnableMongoAuditing
+@EnableRedisRepositories
 @EnableBinding(Channels.class)
 @ComponentScan({"com.stack.taskservice", "com.stack.library"})
 public class Application {
