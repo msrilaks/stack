3e9f03e09b77fe16d4a608623368afd36c1dd8a9
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2020-03-04 18:22:31 -0800

    Stack welcome email

--- stack-services/stack-library/src/main/java/com/stack/library/constants/StackEmailConstants.java
+++ stack-services/stack-library/src/main/java/com/stack/library/constants/StackEmailConstants.java
@@ -18,6 +18,7 @@ public class StackEmailConstants {
         "<a href=\"https://icons8.com/icon/119008/share\">Share icon by Icons8</a>\n" +
         "<a href=\"https://icons8.com/icon/42799/edit\">Edit icon by Icons8</a>\n" +
         "<a href=\"https://icons8.com/icon/60037/google-calendar\">Google Calendar icon by Icons8</a>\n" +
+        "<a href=\"https://icons8.com/icon/12028/price-tag\">Price Tag icon by Icons8</a>"+
         "</div>";
 
     public static final String STACK_CREATED_TOPIC   = "STACK_CREATED";
@@ -25,14 +26,33 @@ public class StackEmailConstants {
     public static final String STACK_CREATED_MESSAGE =
         STACK_MESSAGE_START+
         STACK_MESSAGE_HEADER +
-        "<h2>Hello <b><i>{0}</i></b> !</h2><br/>"+
+        "<h2>Hello, <b><i>{0}</i></b> !</h2><br/>"+
         "<h3>Welcome to <a href=\"http://www.stackitdown.com\" style=\"color:#1ea5ae;\">www.stackitdown.com</a> !</h3>"+
         "<h4>"+
         "<ul style=\"list-style-type:none;\">"+
-        "<li><img src=\"https://img.icons8.com/dusk/32/000000/edit.png\"><i style=\"margin:15px;\">Keep up with those little things to do. Stack all your tasks in one place and free your mind</i></li><br/>"+
-        "<li><img src=\"https://img.icons8.com/cute-clipart/32/000000/share.png\"><i style=\"margin:15px;\">Collaborate, push tasks to your friends</i></li><br/>"+
-        "<li><img src=\"https://img.icons8.com/dusk/32/000000/user-location.png\"><i style=\"margin:15px;\">Get notified, get reminded to run that errand the next time you are in the vicinity</i></li><br/>"+
-        "<li><img src=\"https://img.icons8.com/color/32/000000/google-calendar.png\"><i style=\"margin:15px;\">Add Google Calendar Events</I></li><br/>"+
+        "<li><img src=\"https://img.icons8.com/dusk/32/000000/edit.png\"><i style=\"margin:15px;\">" +
+            "Keep up with those many little things to do. Stack all your tasks in one place" +
+            "and free your mind for more creative thoughts. There is no need to cram a " +
+            "long list of to-do's into your mind or on little scribble notes only to " +
+            "have some of them fall off the back of your mind. Keeping up with an " +
+            "ever-growing, overwhelming list of to-do's has never been easier." +
+            "</i></li><br/>"+
+       "<li><img src=\"https://img.icons8.com/dusk/32/000000/user-location.png\">" +
+            "<i style=\"margin:15px;\">Get notified, get reminded to run an errand when " +
+            "you are in the vicinity. Stack It Down notifies you of tasks that you " +
+            "can complete around your current location.</i></li><br/>"+
+        "<li><img src=\"https://img.icons8.com/cute-clipart/32/000000/share.png\">" +
+            "<i style=\"margin:15px;\">" +
+            "Collaborate, push tasks to your friends, nudge them to get it done" +
+            "</i></li><br/>"+
+        "<li><img src=\"https://img.icons8.com/color/32/000000/price-tag.png\">" +
+            "<i style=\"margin:15px;\">" +
+            "Tag your tasks, organize them into categories and search easily" +
+            "</i></li><br/>"+
+        "<li><img src=\"https://img.icons8.com/color/32/000000/google-calendar.png\">" +
+            "<i style=\"margin:15px;\">" +
+            "Add Google Calendar Events" +
+            "</I></li><br/>"+
         "</ul>"+
         "</h4>"+
         STACK_MESSAGE_FOOTER+
